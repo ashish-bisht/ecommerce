4c828904e877f97508d6d24e62399f4f39f6e26a
ashish <ashishbisht723@gmail.com>
2017-11-21 22:24:33 +0530

    Registration Page complete

--- ecommerce/ecommerce/forms.py
+++ ecommerce/ecommerce/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class ContactForm(forms.Form):
     fullname = forms.CharField(widget = forms.TextInput(attrs={
@@ -46,6 +49,23 @@ class RegisterForm(forms.Form):
         'class':'form-control',
         'placeholder':"Re-Enter password here"
     }))
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        qs = User.objects.filter(username=username)
+        if qs.exists(): 
+            raise forms.ValidationError("Username is taken")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = User.objects.filter(email=email)
+        if qs.exists(): 
+            raise forms.ValidationError("Email is taken")
+        return email
+
+
+
     def clean(self):
         data = self.cleaned_data
         password = self.cleaned_data.get('password')
