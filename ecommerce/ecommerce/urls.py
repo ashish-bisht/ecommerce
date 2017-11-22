1db96e85926021530ba06126d82d97d066635cb5
ashish <ashishbisht723@gmail.com>
2017-11-22 12:35:03 +0530

    Static Files

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.conf.urls import url
 from django.contrib import admin
 from .views import home_page, about_page, contact_page, login_page, register_page
@@ -27,3 +30,7 @@ urlpatterns = [
     
 
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
