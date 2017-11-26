096b54384cd7b285a46e356511f4c97764dbf413
ashish <ashishbisht723@gmail.com>
2017-11-26 18:12:12 +0530

    Model Manager customised

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -19,11 +19,23 @@ def upload_image_path(instance, filename):
             final_filename=final_filename
             )
 
+class ProductManager(models.Manager):  # over written the objects Model Manager
+    def get_by_id(self,id):
+        qs = self.get_queryset().filter(id=id) #Product.objects == self.get_queryset
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
 class Product(models.Model):
     title           = models.CharField(max_length=120)
     description     = models.TextField()
     price           = models.DecimalField(decimal_places=2, max_digits=20, default=29)
     image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    featured        = models.BooleanField(default=False)
+
+    
+    objects = ProductManager()
+
 
     def __str__(self):
         return self.title
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 # Create your views here.
@@ -18,10 +18,19 @@ class ProductListView(ListView):
     
 
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self,*args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args,**kwargs)
         print(context)
         return context
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product Doesnt exist")
+        return instance
+        print(instance)
