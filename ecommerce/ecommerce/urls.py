4c79194aace4f1891a68e55e1d588ade63d36a6d
ashish <ashishbisht723@gmail.com>
2017-11-27 00:00:06 +0530

    slug signal urls

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -13,28 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
+
+
 from .views import home_page, about_page, contact_page, login_page, register_page
-from products.views import ProductListView,ProductDetailView
-
-
 
 urlpatterns = [
+    url(r'^$', home_page),
+    url(r'^about/$', about_page),
+    url(r'^contact/$', contact_page),
+    url(r'^login/$', login_page),
+    url(r'^register/$', register_page),
+    url(r'^products/', include("products.urls")),
     url(r'^admin/', admin.site.urls),
-    url(r'^$',home_page ),
-    url(r'^about/$',about_page ),
-    url(r'^contact/$',contact_page ),
-    url(r'^login/$',login_page),
-    url(r'^register/$',register_page),
-    url(r'^products/$',ProductListView.as_view() ),
-    url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
-    
-
 ]
+
 
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
